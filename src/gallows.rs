ced0f9ee089bd88878617b42309558b32b34e3a2
Giorgi Beriashvili <giorgi.beriashvili@outlook.com>
2020-04-03 05:58:14 +0400

    Add tests

--- src/gallows.rs
+++ src/gallows.rs
@@ -30,3 +30,52 @@ impl Gallows {
         self.phases[phase].depiction.clone()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn setup_gallows() -> Gallows {
+        Gallows::new()
+    }
+
+    #[test]
+    fn test_depict() {
+        let gallows = setup_gallows();
+
+        assert_eq!(
+            "\n_________\n|/      |\n|       O\n|      /|\\\n|      / \\\n|\n|___\n",
+            gallows.depict(6)
+        );
+
+        assert_eq!(
+            "\n_________\n|/      |\n|       O\n|      /|\\\n|      /\n|\n|___\n",
+            gallows.depict(5)
+        );
+
+        assert_eq!(
+            "\n_________\n|/      |\n|       O\n|      /|\\\n|\n|\n|___\n",
+            gallows.depict(4)
+        );
+
+        assert_eq!(
+            "\n_________\n|/      |\n|       O\n|      /|\n|\n|\n|___\n",
+            gallows.depict(3)
+        );
+
+        assert_eq!(
+            "\n_________\n|/      |\n|       O\n|       |\n|\n|\n|___\n",
+            gallows.depict(2)
+        );
+
+        assert_eq!(
+            "\n_________\n|/      |\n|       O\n|\n|\n|\n|___\n",
+            gallows.depict(1)
+        );
+
+        assert_eq!(
+            "\n_________\n|/      |\n|\n|\n|\n|\n|___\n",
+            gallows.depict(0)
+        );
+    }
+}
